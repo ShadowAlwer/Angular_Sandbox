bd44fd5104c035b9e3857014d5dcebdfbf1db704
Alex Porębski <porebski.alex@gmail.com>
2023-08-20 14:06:44 +0200

    dokumentacja

--- src/app/champ-details/champ-details.component.ts
+++ src/app/champ-details/champ-details.component.ts
@@ -8,16 +8,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ChampDetailsComponent {
 
-  // http://ddragon.leagueoflegends.com/cdn/13.16.1/data/en_US/champion/ + <champName.json>
+  // Dokumentacja: https://developer.riotgames.com/docs/lol#data-dragon_champions
+
+  // http://ddragon.leagueoflegends.com/cdn/13.16.1/data/en_US/champion/ + <champId.json>
   
   public champId: string = "";
-
 
   constructor(
     private activatedRoute: ActivatedRoute
   ) {
     console.log(this.activatedRoute.snapshot)
     this.champId = this.activatedRoute.snapshot.params['champId'];
-    
+
   }
 }
