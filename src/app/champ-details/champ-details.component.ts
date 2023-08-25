6f505191e0d1cb6fe372b8cbda2e3651cbb08139
Alex Porębski <porebski.alex@gmail.com>
2023-08-25 17:57:38 +0200

    fixes

--- src/app/champ-details/champ-details.component.ts
+++ src/app/champ-details/champ-details.component.ts
@@ -13,32 +13,39 @@ export class ChampDetailsComponent implements OnInit {
 
   // http://ddragon.leagueoflegends.com/cdn/13.16.1/data/en_US/champion/ + <champId.json>
 
-  public champsData: any = {};
+  public champsDetails: any = null;
   public champId: string = "";
   public passiveName: string = "";
-
-  ngOnInit(): void {
-    console.log("Request data")
-    this.http.get<any>('http://ddragon.leagueoflegends.com/cdn/13.16.1/data/en_US/champion/' + this.champId + '.json')
-      .subscribe((data: any) => {
-          this.champsData = data;
-          console.log("Retrieved champs data: ", this.champsData);
-      })
-    console.log("After request data")
-  }
 
   constructor(
     private http: HttpClient,
     private activatedRoute: ActivatedRoute
   ) {
-    
     console.log(this.activatedRoute.snapshot)
     this.champId = this.activatedRoute.snapshot.params['champId'];
+    // this.champId = 'Ashe' 
     console.log(this.champId);
-
-    console.log(this.activatedRoute.snapshot)
-    this.passiveName = this.activatedRoute.snapshot.params['passiveName'];
-    console.log(this.passiveName);
-
   }
+
+  ngOnInit(): void {
+    console.log("Request data")
+    this.http.get<any>('http://ddragon.leagueoflegends.com/cdn/13.16.1/data/en_US/champion/' + this.champId + '.json')
+      .subscribe((details: any) => {
+          this.champsDetails = details;
+          this.passiveName = this.champsDetails.data[this.champId].passive.image.full;
+                            // this.champsData.data.Ahri.passive.image.full;
+                            // this.champsDetails.data[this.champId].spell[0]
+                            // let index = 2;
+                            // index++
+                            // for (let index = 0; index < 4; index++ ) {
+                            //    this.champsDetails.data[this.champId].spell[index].description
+                            // }
+          console.log("Retrieved champs data: ",);
+          console.dir(this.champsDetails);
+      })
+    console.log("After request data")
+  }
+ 
+
+
 }
