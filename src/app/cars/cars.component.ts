264351c8caa5986aa528af81f3532b9b5f1f7bbf
Alex Porębski <porebski.alex@gmail.com>
2023-09-29 18:49:42 +0200

    input refactor

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,46 +7,13 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-  public enterOwner: string = '';
-  public enterName: string = '';
-  public enterYear: string = '';
-  public enterModel: string = '';
-  public enterColor: string = '';
+  public owner: string = '';
+  public modelName: string = '';
+  public yearOfProduction: string = '';
+  public mark: string = '';
+  public color: string = '';
 
-  displayOwner = '';
-  displayName = '';
-  displayYear = '';
-  displayModel = '';
-  displayColor = '';
-
-  i1 = [this.enterOwner];
-  i2 = [this.enterName];
-  i3 = [this.enterYear];
-  i4 = [this.enterModel];
-  i5 = [this.enterColor];
-
-  getOwner(val: string){
-    console.warn(val)
-    this.displayOwner = val;
-  }
-
-  getName(val: string){
-    console.warn(val)
-    this.displayName = val;
-  }
-
-  getYear(val: string){
-    console.warn(val)
-    this.displayYear = val;
-  }
-
-  getModel(val: string){
-    console.warn(val)
-    this.displayModel = val;
-  }
-
-  getColor(val: string){
-    console.warn(val)
-    this.displayColor = val;
+  submit() {
+    // TODO;
   }
 }
