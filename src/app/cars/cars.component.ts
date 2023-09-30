47ada782897130254f5c95810f90de6c5820d568
Patryk <patryk.porebski@gmail.com>
2023-09-30 12:12:05 +0200

    CARS - FIX

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -13,40 +13,32 @@ export class CarsComponent {
   public enterModel: string = '';
   public enterColor: string = '';
 
-  displayOwner = '';
-  displayName = '';
-  displayYear = '';
-  displayModel = '';
-  displayColor = '';
+  public cars: Car[] = [
+    {
+      Owner: 'Jan Kowalski',
+      Name: 'Buka',
+      Year: '1989 r.',
+      Model: 'Audi',
+      Color: 'red'
+    },
+    {
+      Owner: 'Adam Nowak',
+      Name: 'Mumin',
+      Year: '1973 r.',
+      Model: 'BMW',
+      Color: 'black'
+    },
+  ]
 
-  i1 = [this.enterOwner];
-  i2 = [this.enterName];
-  i3 = [this.enterYear];
-  i4 = [this.enterModel];
-  i5 = [this.enterColor];
+  submit() {
+    const newCar = {
+      Owner: this.enterOwner,
+      Name: this.enterName,
+      Year: this.enterYear,
+      Model: this.enterModel,
+      Color: this.enterColor,
+    }
 
-  getOwner(val: string){
-    console.warn(val)
-    this.displayOwner = val;
-  }
-
-  getName(val: string){
-    console.warn(val)
-    this.displayName = val;
-  }
-
-  getYear(val: string){
-    console.warn(val)
-    this.displayYear = val;
-  }
-
-  getModel(val: string){
-    console.warn(val)
-    this.displayModel = val;
-  }
-
-  getColor(val: string){
-    console.warn(val)
-    this.displayColor = val;
+    this.cars.push(newCar);
   }
 }
